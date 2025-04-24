4335b01dd9b2bbf0361ffd4c1205ba6eb7bd78fd
Hunter Scoggins <hscoggins5018@gmail.com>
2025-04-24 09:49:33 -0500

    correcting heading

--- src/app/page.js
+++ src/app/page.js
@@ -103,7 +103,7 @@ export default function Game() {
 
   return (
     <div className="game">
-      <h1>Testiing auto deployment</h1>
+      <h1>Testing auto deployment</h1>
       <div className="game-board">
         <Board squares={currentSquares} xIsNext={xIsNext} onPlay={handlePlay} />
       </div>
